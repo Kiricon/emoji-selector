79ee1dcf63172211816f64ca51fcd65d42a940b8
Dominic Valenciana <kiricon@live.com>
2017-11-11 21:17:21 -0700

    made button an emoji icon

--- emoji-selector.ts
+++ emoji-selector.ts
@@ -107,8 +107,22 @@ template.innerHTML = `
         fun-tab:hover, .emoji:hover {
             background: #eee;
         }
+
+        button {
+            background: none;
+            border: none;
+            cursor: pointer;
+            padding: 0px;
+            height: 30px;
+            width: 30px;
+        }
     </style>
-    <button>open</button>
+    <button>
+        <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 0h24v24H0z" fill="none"/>
+            <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/>
+        </svg>
+    </button>
     <div id="emojiPopup">
         <fun-tabs selected="0">${tabTemplate}</fun-tabs>
         <div class="content">${contentTemplate}</div>
@@ -173,8 +187,8 @@ class EmojiSelector extends HTMLElement {
         this.openButton.addEventListener('click', () => {
             
             const offset = this.findTopLeft(this.openButton);
-            this.popupWindow.style.top = (offset.top-150)+'px';
-            this.popupWindow.style.left = (offset.left-150)+'px';
+            this.popupWindow.style.top = (offset.top-150+15)+'px';
+            this.popupWindow.style.left = (offset.left-150+15)+'px';
             this.popupWindow.className = 'open';
         });
     }
